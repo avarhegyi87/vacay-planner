43de4aaba67d1486724cf5e0f760a0ece849f90a
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2023-11-10 16:32:16 +0100

    feat: routes for local auth strategy

--- apps/backend/src/routes/auth-routes.ts
+++ apps/backend/src/routes/auth-routes.ts
@@ -1,7 +1,33 @@
 import express from 'express';
 import passport from 'passport';
+import bcrypt from 'bcryptjs';
+import User from '../sql/models/user';
 
 export const router = express.Router();
+
+router.post('/api/register', (req, res) => {
+  const { username, email, password } = req.body;
+
+  bcrypt.hash(password, 10, async (err, hash) => {
+    if (err) throw err;
+
+    const newUser = new User({ email: email, password: hash });
+    await newUser
+      .save()
+      .then(() => res.redirect('/'))
+      .catch(err => {
+        return err;
+      });
+  });
+});
+
+router.post(
+  '/api/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/api/login',
+  })
+);
 
 router.get(
   '/auth/google',
