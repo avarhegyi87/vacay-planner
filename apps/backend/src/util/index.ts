e58e927c368e177cce55271d751163274d0b0fac
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2023-11-23 13:55:38 +0100

    BE: tokenSender, rearrange utils and middlewares

---
+++ apps/backend/src/util/index.ts
@@ -0,0 +1,2 @@
+export * from './get-active-user-id';
+export * from './token-sender';
---
+++ apps/backend/src/util/token-sender.ts
@@ -0,0 +1,28 @@
+import nodemailer from 'nodemailer';
+import sgMail from '@sendgrid/mail';
+
+export async function sendToken(userEmail: string, token: string) {
+  try {
+    if (!process.env.SENDGRID_API_KEY)
+      throw Error('Sendgrid API key missing, no email sent');
+
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const mailOptions = {
+      from: process.env.SENDGRID_EMAIL ?? 'avarhegyi87@gmail.com',
+      to: userEmail,
+      subject: 'VacayPlanner - confirm your registration',
+      html: `<h1>Hi!</h1>
+    <p>Thanks for registering to VacayPlanner.</p>
+    <p>Please follow <a href="http://localhost:4200/api/auth/verify/${token}"><strong>THIS LINK</strong></a> to verify your email address. See you around!</p>`,
+    };
+
+    await sgMail.send(mailOptions);
+    console.log('Verification email sent');
+  } catch (error: any) {
+    console.error(error);
+    throw Error(error.message);
+  }
+}
+
+export default sendToken;
