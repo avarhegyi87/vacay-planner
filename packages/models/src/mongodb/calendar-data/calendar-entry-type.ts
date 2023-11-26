484efcab3473845838e946d756f5277e6c108a6b
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2023-11-26 19:14:58 +0100

    update calendar entry types

--- packages/models/src/mongodb/calendar-data/calendar-entry-type.ts
+++ packages/models/src/mongodb/calendar-data/calendar-entry-type.ts
@@ -12,10 +12,10 @@ export enum CalendarEntryTypeEnum {
   homeOffice = 'home office',
   workAtOffice = 'work at office',
   workAtClient = 'work at client',
-  shift1 = 'shift 1',
-  shift2 = 'shift 2',
-  shift3 = 'shift 3',
-  shift4 = 'shift 4',
+  task1 = 'task 1',
+  task2 = 'task 2',
+  task3 = 'task 3',
+  task4 = 'task 4',
   EMPTY = 'EMPTY',
 }
 
@@ -30,7 +30,7 @@ export const calendarEntryInfo: Array<ICalendarEntryInfo> = [
   { name: CalendarEntryTypeEnum.EMPTY, sign: '', isAbsence: false, isVacation: false },
 
   { name: CalendarEntryTypeEnum.vacation, sign: 'V', isAbsence: true, isVacation: true },
-  { name: CalendarEntryTypeEnum.plannedVacation, sign: 'PV', isAbsence: true, isVacation: true },
+  { name: CalendarEntryTypeEnum.plannedVacation, sign: 'V?', isAbsence: true, isVacation: true },
   { name: CalendarEntryTypeEnum.halfDayVacation, sign: 'HV', isAbsence: true, isVacation: true },
   { name: CalendarEntryTypeEnum.otherVacation, sign: 'OV', isAbsence: true, isVacation: true },
 
@@ -44,8 +44,8 @@ export const calendarEntryInfo: Array<ICalendarEntryInfo> = [
   { name: CalendarEntryTypeEnum.homeOffice, sign: 'HO', isAbsence: false, isVacation: false },
   { name: CalendarEntryTypeEnum.workAtOffice, sign: 'W@O', isAbsence: false, isVacation: false },
   { name: CalendarEntryTypeEnum.workAtClient, sign: 'W@C', isAbsence: false, isVacation: false },
-  { name: CalendarEntryTypeEnum.shift1, sign: '1', isAbsence: false, isVacation: false },
-  { name: CalendarEntryTypeEnum.shift2, sign: '2', isAbsence: false, isVacation: false },
-  { name: CalendarEntryTypeEnum.shift3, sign: '3', isAbsence: false, isVacation: false },
-  { name: CalendarEntryTypeEnum.shift4, sign: '4', isAbsence: false, isVacation: false },
+  { name: CalendarEntryTypeEnum.task1, sign: '1', isAbsence: false, isVacation: false },
+  { name: CalendarEntryTypeEnum.task2, sign: '2', isAbsence: false, isVacation: false },
+  { name: CalendarEntryTypeEnum.task3, sign: '3', isAbsence: false, isVacation: false },
+  { name: CalendarEntryTypeEnum.task4, sign: '4', isAbsence: false, isVacation: false },
 ];
