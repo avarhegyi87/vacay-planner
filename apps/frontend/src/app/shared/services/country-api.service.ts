ce5a159cec98c298a54062185008b598ac8c288e
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2023-11-26 17:27:44 +0100

    fix: route to get public holiday API key

--- apps/frontend/src/app/shared/services/country-api.service.ts
+++ apps/frontend/src/app/shared/services/country-api.service.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of, tap } from 'rxjs';
+import { Observable, catchError, of, switchMap, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class CountryApiService {
@@ -21,16 +21,31 @@ export class CountryApiService {
     );
   }
 
+  getPublicHolidayAPIkey(): Observable<string> {
+    return this.http.get<string>('/api/calendars/public-holiday-api-key').pipe(
+      catchError(() => of('')),
+    );
+  }
+
   getPublicHolidays(year: number, countryCode: string): Observable<any> {
     if (this.publicHolidayCache[countryCode]) return of(this.publicHolidayCache[countryCode]);
+    
+    return this.getPublicHolidayAPIkey().pipe(
+      catchError(error => {
+        console.error('Error fetching Public Holiday API key', error);
+        return of('');
+      }),
+      switchMap(apiKey => {
+        if (!apiKey) return of(null);
 
-    return this.http
-      .get(`https://public-holiday.p.rapidapi.com/${year}/${countryCode}`, {
-        headers: {
-          'X-RapidAPI-Key': process.env['PUBLIC_HOLIDAY_API_KEY'] ?? '',
-          'X-RapidAPI-Host': 'public-holiday.p.rapidapi.com',
-        },
-      })
-      .pipe(tap(holidays => (this.publicHolidayCache[countryCode] = holidays)));
+        return this.http
+          .get(`https://public-holiday.p.rapidapi.com/${year}/${countryCode}`, {
+            headers: {
+              'X-RapidAPI-Key': apiKey ?? '',
+              'X-RapidAPI-Host': 'public-holiday.p.rapidapi.com',
+            },
+          }).pipe(tap(holidays => (this.publicHolidayCache[countryCode] = holidays)));
+      }),
+    );
   }
 }
